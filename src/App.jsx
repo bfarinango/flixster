45568952f19c8b1ac99b326dfb3b88f77e17aa22
bfarinango <briannafarinango3@gmail.com>
2025-06-20 14:01:38 -0700

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ import MovieList from "./components/MovieList/MovieList";
 
 const App = () => {
   return (
+
     <div className="App">
-      <MovieList />
+      <header className="App-header">
+        <h1>Flixster</h1>
+      </header>
+
+      <main>
+        <MovieList />
+      </main>
+
+      <footer className="App-footer">
+        <p>By: Brianna Farinango</p>
+      </footer>
     </div>
   );
 };
